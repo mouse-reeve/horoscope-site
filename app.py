8b9642d638b040b1c1a1e72c78310f940cb42be3
Mouse Reeve <mouse.reeve@gmail.com>
2015-10-05 17:12:40 -0700

    Formatting

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 ''' a generative horoscope site '''
-from flask import Flask
+from flask import Flask, render_template
 from horoscope_generator import HoroscopeGenerator
 import json
 import random
@@ -8,13 +8,45 @@ import random
 APP = Flask(__name__)
 
 
+# ROUTES
+@APP.route('/')
+def index():
+    ''' render the basic template for angular '''
+    return render_template('index.html', data=horoscope_data())
+
+
 @APP.route('/api/horoscope', methods=['GET'])
 def get_horoscope():
+    ''' return horoscope data to client '''
+    return json.dumps(horoscope_data())
+
+
+def horoscope_data():
     ''' Get a generated horoscope '''
-    sentences = []
-    for _ in range(random.randint(3, 7)):
-        sentences.append(HoroscopeGenerator.format_sentence(HoroscopeGenerator.get_sentence()))
-    return json.dumps({'horoscope': '%s.' % '. '.join(sentences)})
+
+    animals = ['hermit crab', 'puppy', 'seagull', 'sea slug']
+    icons = [
+        u'\u263C', # sun
+        u'\u2694', # swords
+        u'\u2696', # scales
+        u'\u00F8', # o slash
+        u'\u2741', # flower
+        u'\u26B1', # urn
+        u'\u269A', # staff of hermes
+        u'\u27E1', # concave diamond
+        u'\u2932', # crosses arrows
+    ]
+
+    horoscope = HoroscopeGenerator.format_sentence(HoroscopeGenerator.get_sentence())
+
+    options = [str(i) for i in range(0, 9)] + ['A', 'B', 'C', 'D', 'E', 'F']
+    color = [random.choice(options) for _ in range(6)]
+    return {
+        'horoscope': horoscope,
+        'animal': random.choice(animals),
+        'icon': random.choice(icons),
+        'color': ''.join(color)
+    }
 
 
 if __name__ == '__main__':
